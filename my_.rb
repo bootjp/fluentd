3e1cfdfc0fb80cab89ee2b71b25dc3f4116794c0
bootjp <oh@bootjp.me>
2019-07-10 16:28:05 +0900

    wip commits
    Signed-off-by: bootjp <oh@bootjp.me>

--- my_.rb
+++ my_.rb
@@ -16,6 +16,10 @@ SRV = Struct.new(
       sock.close rescue nil
     end
   end
+  # fixme more think. sort srv embedded?
+  # def <=> (b)
+  #   :priority < b.priority || (:priority == b.priority && :weight < b.weight)
+  # end
 end
 
 def srv_list_sort_priority_weight(srv_list)
@@ -37,21 +41,17 @@ def srv_list_sort_priority_weight(srv_list)
   current = 1
 
   ret = []
+  srv_list.sort_by!(&:weight)
   srv_list.sort_by!(&:priority)
-  # pp srv_list
+
   until (srv_list.size - 1) < current do
-    # pp last, current
-    # pp srv_list[last]['priority'] != srv_list[current]['priority']
     if srv_list[last]['priority'] != srv_list[current]['priority']
       ret.push weight_by_shuffle srv_list[last...current]
-      # pp "---"
       last = current
     end
     current += 1
   end
-  ret.push weight_by_shuffle srv_list[last...current] # weight sort)
-  # ret =
-
+  ret.push weight_by_shuffle srv_list[last...current]
   ret
 end
 
@@ -59,10 +59,6 @@ def weight_by_shuffle(srv_list)
   if srv_list.nil? || srv_list.empty?
     return []
   end
-
-  # if srv_list.length == 1
-  #   return srv_list
-  # end
 
   # Compute the sum of the weights of those RRs, and with each RR
   # associate the running sum in the selected order
@@ -73,38 +69,54 @@ def weight_by_shuffle(srv_list)
   # (inclusive), and select the RR whose running sum value is the
   # first in the selected order which is greater than or equal to
   # the random number selected.
-  pp srv_list
 
-  while sum > 0 && srv_list.length > 1
-    pp "loop"
+  until srv_list.empty?
+    # Then choose a uniform random number between 0 and the sum computed (inclusive),
     selector = Integer(rand(sum))
-    selected_sum = 0
-    srv_list.each.with_index do |srv, index|
-      selected_sum += srv.weight
-      if selected_sum > selector
+    running_sum = 0
+
+    # todo 順序付けされていないSRV RRがなくなるまで、順序付けプロセスを続けます。 このプロセスは優先度ごとに繰り返されます。
+    srv_list.each_index do |index|
+      running_sum += srv_list[index].weight
+      # and select the RR whose running sum value is the first in the selected order which is greater than or equal to the random number selected.
+      if running_sum > selector
         if index > 0
-          pp "index",  index
-          srv_list[0], srv_list[index] = srv_list[index], srv_list[0]
+          # The target host specified in the selected SRV RR is the next one to be contacted by the client.
+          srv_list[0] = srv_list[index]
+          srv_list[index] = srv_list[0]
         end
+        break
       end
     end
-    pp srv_list
-    # fixme
-    sum -= srv_list[0].weight
-    srv_list = srv_list[0..-1]
   end
-
   srv_list
 end
 
 ret = srv_list_sort_priority_weight(
     [
-      SRV.new(0, 100, 22424, "localhost"),
-      SRV.new(2, 10, 22424, "localhost"),
+      SRV.new(0, 100, 22424, "aas"),
+      SRV.new(2, 20, 22424, "cc"),
+      SRV.new(2, 10, 22424, "aa"),
       SRV.new(4, 100, 22424, "localhost"),
-      SRV.new(2, 100, 22424, "localhost"),
+      SRV.new(2, 100, 22424, "bnbb"),
     ]
 )
+#
+#
+# ret = weight_by_shuffle(
+#     [
+#       # SRV.new(0, 100, 22424, "localhost"),
+#         SRV.new(2, 30, 22424, "vvvv"),
+#       SRV.new(2, 60, 22424, "localhost"),
+#       SRV.new(2, 40, 22424, "localhost"),
+#         # SRV.new(2, 30, 22424, "vvvv"),
+#       # SRV.new(2, 100, 22424, "localhost"),
+#     ]
+# =begin
+#     ].shuffle!
+# =end
+# )
+# #
 pp "--"
 
 pp ret
