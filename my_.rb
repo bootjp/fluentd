7de51540ef73654244e99d4c93ec54814c6cd3a8
bootjp <oh@bootjp.me>
2019-07-12 12:21:12 +0900

    add not zero weight shuffle and refactor
    Signed-off-by: bootjp <oh@bootjp.me>

--- my_.rb
+++ my_.rb
@@ -16,12 +16,9 @@ SRV = Struct.new(
       sock.close rescue nil
     end
   end
-  # fixme more think. sort srv embedded?
-  # def <=> (b)
-  #   :priority < b.priority || (:priority == b.priority && :weight < b.weight)
-  # end
 end
 
+## TODO ADD TEST
 def srv_list_sort_priority_weight(srv_list)
   if srv_list.nil? || srv_list.empty?
     return []
@@ -55,6 +52,34 @@ def srv_list_sort_priority_weight(srv_list)
   ret
 end
 
+## TODO ADD TEST
+def weight_shuffle(srv_list)
+  if srv_list.nil? || srv_list.empty?
+    return []
+  end
+
+  # To select a target to be contacted next, arrange all SRV RRs
+  #  (that have not been ordered yet) in any order, except that all
+  # those with weight 0 are placed at the beginning of the list.
+  shuffle_start = nil
+  srv_list.sort_by!(&:weight)
+  srv_list.each_index do |index|
+    if srv_list[index].weight != 0
+      if shuffle_start.nil?
+        shuffle_start = index
+      end
+      if !shuffle_start.nil? && srv_list[index].weight != 0
+        shuffle_end = index
+        target = srv_list.dup
+        srv_list[shuffle_start..shuffle_end] = target[shuffle_start..shuffle_end].shuffle!
+      end
+    end
+  end
+
+  srv_list
+end
+
+## TODO ADD TEST
 def weight_by_shuffle(srv_list)
   if srv_list.nil? || srv_list.empty?
     return []
@@ -64,40 +89,37 @@ def weight_by_shuffle(srv_list)
   # associate the running sum in the selected order
   sum = srv_list.inject(0) { |sum, srv| sum + srv.weight}
 
-  # Then choose a
-  # uniform random number between 0 and the sum computed
-  # (inclusive), and select the RR whose running sum value is the
-  # first in the selected order which is greater than or equal to
-  # the random number selected.
-
-  until srv_list.empty?
+  srv_list = weight_shuffle(srv_list)
+  ret = []
+  # Then choose a uniform random number between 0 and the sum computed (inclusive), and select the RR whose running sum value is the first in the selected order which is greater than or equal to the random number selected.
+  until sum <= 0 || srv_list.length == 0
     # Then choose a uniform random number between 0 and the sum computed (inclusive),
     selector = Integer(rand(sum))
     running_sum = 0
 
-    # todo 順序付けされていないSRV RRがなくなるまで、順序付けプロセスを続けます。 このプロセスは優先度ごとに繰り返されます。
     srv_list.each_index do |index|
       running_sum += srv_list[index].weight
       # and select the RR whose running sum value is the first in the selected order which is greater than or equal to the random number selected.
       if running_sum > selector
-        if index > 0
-          # The target host specified in the selected SRV RR is the next one to be contacted by the client.
-          srv_list[0] = srv_list[index]
-          srv_list[index] = srv_list[0]
-        end
+        ret.push srv_list[index]
+        sum -= srv_list[index].weight
+        srv_list.delete_at index
         break
       end
     end
   end
-  srv_list
+  ret
 end
+
+
 
 ret = srv_list_sort_priority_weight(
     [
-      SRV.new(0, 100, 22424, "aas"),
+      # SRV.new(0, 100, 22424, "aas"),
+      SRV.new(2, 0, 22424, "d"),
       SRV.new(2, 20, 22424, "cc"),
       SRV.new(2, 10, 22424, "aa"),
-      SRV.new(4, 100, 22424, "localhost"),
+      # SRV.new(4, 100, 22424, "localhost"),
       SRV.new(2, 100, 22424, "bnbb"),
     ]
 )
